ca8b8af7d73a8873b1062fdac10a5d1b891ad4b0
hilmyfar <mistercapcay@gmail.com>
2021-07-16 23:03:38 +0700

    initial commit

---
+++ data.js
@@ -0,0 +1,365 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bird-view-depan",
+      "name": "Bird View Depan",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6626097399473831,
+        "pitch": 0.16452048349803938,
+        "fov": 1.3856270634747951
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6285073663237526,
+          "pitch": 0.2285758480069333,
+          "rotation": 0,
+          "target": "1-depan-pintu-masuk"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8737715331985498,
+          "pitch": 0.4551412659010552,
+          "title": "Letak geografis",
+          "text": "Alun-alun Manunggal Daya terletak di Kutai Kartanegara, Kalimantan Timur. Pembangunan alun-alun ini merupakan salah satu rencana pembangunan ibu kota baru Indonesia."
+        },
+        {
+          "yaw": -1.2762025533555743,
+          "pitch": 0.26298182317446717,
+          "title": "SMPN 2 Sebulu",
+          "text": "Kutai Kartanegara"
+        }
+      ]
+    },
+    {
+      "id": "1-depan-pintu-masuk",
+      "name": "Depan pintu masuk",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.668878084062328,
+        "pitch": -0.0021816943678842904,
+        "fov": 1.3856270634747951
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5463216542435294,
+          "pitch": 0.06510733912915967,
+          "rotation": 0,
+          "target": "2-pertigaan-papan-informasi"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8236139321410025,
+          "pitch": 0.08621602339404077,
+          "title": "Disclaimer",
+          "text": "Render 3D dari konsep alun-alun ini adalah fiksi dan tidak merepresentasikan rencana/rancangan pembangunan alun-alun ini. Render 3D ini hanyalah untuk keperluan tugas mata kuliah Komputasi Perencanaan dan sepenuhnya merupakan hak milik dari Hilmy, baik dari segi konsep maupun produk yang dihasilkan."
+        },
+        {
+          "yaw": 1.2344872395842668,
+          "pitch": 0.08296718411176762,
+          "title": "Software in use",
+          "text": ".Sketchup 2018<div>.Enscape</div><div>.Marzipano</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-pertigaan-papan-informasi",
+      "name": "Pertigaan papan informasi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6984944972317368,
+        "pitch": 0.11453596609289107,
+        "fov": 1.3856270634747951
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8092834158789124,
+          "pitch": 0.007176593054076363,
+          "rotation": 0,
+          "target": "3-teras-danau"
+        },
+        {
+          "yaw": 0.48515796047481174,
+          "pitch": 0.021843528171729076,
+          "rotation": 0,
+          "target": "4-amphiteater"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7722834683202713,
+          "pitch": 0.20572427546072092,
+          "title": "Konsep Alun-alun",
+          "text": "Konsep Pembangunan Alun-alun Manunggal Daya yang ideal menurut Hilmy adalah dengan menjadikannya sebagai tempat rekreasi terpadu all-in-one. Memadukan tempat berkumpul keluarga, kegiatan warga, dan olahraga."
+        },
+        {
+          "yaw": 1.279225270297827,
+          "pitch": 0.1431202419926052,
+          "title": "List Fasilitas",
+          "text": "-Amphiteater<div>-Area piknik</div><div>-Taman bermain</div><div>-Teras danau</div><div>-Mini football</div><div>-Kantin</div><div>-Toilet</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-teras-danau",
+      "name": "Teras Danau",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6010936921699521,
+        "pitch": 0.00533306560112301,
+        "fov": 1.3856270634747951
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5114575200465659,
+          "pitch": 0.01951177584485464,
+          "rotation": 0,
+          "target": "2-pertigaan-papan-informasi"
+        },
+        {
+          "yaw": 1.9158953202386098,
+          "pitch": 0.008952207149430791,
+          "rotation": 0,
+          "target": "5-pertigaan-kantin"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6758371154639216,
+          "pitch": 0.2669592587713403,
+          "title": "Teras Danau",
+          "text": "<span lang=\"EN-US\" style=\"font-size:12.0pt;line-height:\n107%;font-family:Raleway;mso-fareast-font-family:Calibri;mso-fareast-theme-font:\nminor-latin;mso-bidi-font-family:&quot;Times New Roman&quot;;mso-bidi-theme-font:minor-bidi;\nmso-ansi-language:EN-US;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\">Di\ndepan danau terdapat teras danau, yaitu dudukan bermodel terasering sebagai\nsalah satu spot menikmati pemandangan alun-alun. sekaligus sebagai tempat berkumpul masyarakat dengan banyaknya tempat duduk yang tersedia.</span>"
+        },
+        {
+          "yaw": 3.0510380144450764,
+          "pitch": 0.007024344374542579,
+          "title": "Tempat Parkir",
+          "text": "Terdapat tempat parkir bagi kendaraan roda empat dan roda dua"
+        },
+        {
+          "yaw": -2.3892000417496035,
+          "pitch": 0.23162419374063248,
+          "title": "Dudukan berundak",
+          "text": "Tersedia dudukan berundak yang terinspirasi dari terasering. Dudukan ini menjadi multiguna karena selain menjadi tempat duduk juga dapat menjadi fasilitas olahraga ringan dan hal lainnya."
+        }
+      ]
+    },
+    {
+      "id": "4-amphiteater",
+      "name": "Amphiteater",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4965334949823372,
+        "pitch": 0.0490112138438743,
+        "fov": 1.3856270634747951
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1642072167349173,
+          "pitch": 0.015086580149072049,
+          "rotation": 0,
+          "target": "2-pertigaan-papan-informasi"
+        },
+        {
+          "yaw": 1.2081802511723403,
+          "pitch": -0.006641254127613294,
+          "rotation": 0,
+          "target": "5-pertigaan-kantin"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4369279608339722,
+          "pitch": 0.30871928464043563,
+          "title": "Amphiteater",
+          "text": "<span lang=\"EN-US\" style=\"font-size:12.0pt;line-height:\n107%;font-family:Raleway;mso-fareast-font-family:Calibri;mso-fareast-theme-font:\nminor-latin;mso-bidi-font-family:&quot;Times New Roman&quot;;mso-bidi-theme-font:minor-bidi;\nmso-ansi-language:EN-US;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\">Terdapat\namfiteater yang selain berguna sebagi pusat pertunjukan seni, budaya, ataupun\norasi, juga dapat berguna sebagai tempat duduk-duduk santai, berkumpul, dan\nbercengkrama.&nbsp;</span>"
+        },
+        {
+          "yaw": -3.0850169997789045,
+          "pitch": 0.08273991284695903,
+          "title": "Area piknik",
+          "text": "Rerumputan luas yang dapat dipakai untuk piknik/botram kerabat."
+        },
+        {
+          "yaw": 0.21372086084566178,
+          "pitch": 0.06282648572365268,
+          "title": "Taman bermain",
+          "text": "Fasilitas bermain untuk anak-anak"
+        }
+      ]
+    },
+    {
+      "id": "5-pertigaan-kantin",
+      "name": "Pertigaan kantin",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5485888436057529,
+        "pitch": 0.0043633887357774626,
+        "fov": 1.3856270634747951
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2474156694534724,
+          "pitch": 0.01640082885210603,
+          "rotation": 0,
+          "target": "4-amphiteater"
+        },
+        {
+          "yaw": 0.863809218974529,
+          "pitch": 0.019962727758290555,
+          "rotation": 0,
+          "target": "3-teras-danau"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.104182830788506,
+          "pitch": 0.13937412135713956,
+          "title": "Mini Football",
+          "text": "Sarana olahraga dan hiburan masyarakat sekitar. Dilengkapi oleh tribun kecil bagi penonton"
+        },
+        {
+          "yaw": -1.653379616279917,
+          "pitch": 0.08330299075732128,
+          "title": "Kantin",
+          "text": "Penjual di kantin ini digerakkan dari usaha masyarakat sekitar sebagai upaya memajukan perekonomian sekitar. Sumber listrik dari kantin ini ditenagai oleh tenaga surya dari panel surya yang terletak di atas kantin."
+        },
+        {
+          "yaw": -0.5396823858584963,
+          "pitch": 0.024370692072125166,
+          "title": "Toilet",
+          "text": "Toilet ini merupakan toilet yang ramah lingkungan dengan sumber air yang berasal langsung dari reservoir danau alun-alun ini."
+        },
+        {
+          "yaw": 1.3897146939619844,
+          "pitch": 0.19568463574425188,
+          "title": "Danau",
+          "text": "Danau ini selain mempunyai fungsi estetika juga mempunyai fungsionalitas yang tinggi. Danau ini dapat menjadi tempat reservoir dari air hujan yang terserap dari ruang hijau di sekelilingnya. Selain itu, keberadaan danau juga menjadikan area menjadi lebih sejuk."
+        }
+      ]
+    }
+  ],
+  "name": "Konsep Alun-alun Manunggal Daya -- Hilmy",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
